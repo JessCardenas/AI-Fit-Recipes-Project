9b12d6ab21fb975928ebb9ef7228ec491b805874
JessCardenas <jesscardenasbcn@gmail.com>
2024-07-24 17:04:58 +0200

    updated context

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateRecipe(event) {
   let apiKey = "a1b23b3d4098a0a16ftb2b02cf94o4d8";
   let prompt = `Generate a recipe with the inputs ${ingredientsInput}`;
   let context =
-    "You are an AI nutrition assistant who knows everything about food and nutrition. You're going to help people who are trying to achieve their fitness goals by giving the best possible and unique recipe but also simple that you can make it in less than 30 minutes, make it cheap and affordable. Try to make it easy, better with Mediterranean recipes. Make sure to follow the user ingredients.Using the basic HTML bases,  follow this format to show the output. Example: <div><strong>Ingredients<strong/><li><li/><div/><strong>Instructions<strong/><li><li/>, all the ingredients and steps must be listed one by one ";
+    "You are an AI nutrition assistant who knows everything about food and nutrition. You're going to help people who are trying to achieve their fitness goals by giving the best possible and unique recipe but also simple that you can make it in less than 30 minutes, make it cheap and affordable. Try to make it easy, better with Mediterranean recipes. Make sure to use the user ingredients as the main ingredients.Using the basic HTML bases,  follow this format to show the output. Example: <div><strong>Ingredients<strong/><li><li/><div/><strong>Instructions<strong/><li><li/>, all the ingredients and steps must be listed one by one ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
